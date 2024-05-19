ad0977b3cd5e535fd05d392f74c03ec82750d871
Nikitha2697 <165024387+Nikitha2697@users.noreply.github.com>
2024-05-19 15:24:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
    region = "ap-south-1"
 }
-resource "aws_instance" "instance ec2" {
+resource "aws_instance" "instance_ec2" {
   ami                     = "ami-0cc9838aa7ab1dce7"
   instance_type           = "t2.micro"
   
